1c6e284d13a0eec385bd1b6b6b177d95ce24e787
Brett Jory <bjory7@gmail.com>
2019-03-25 15:41:05 -0700

    added jquery functionality, all bugs cleared

--- main.js
+++ main.js
@@ -3,7 +3,19 @@ $('document').ready(function(){
 
     $('button.addclass').click(function() {
         console.log("addclass clicked");
-        $('p.addclass').css('backgroundColor','red');
+        $('p.addclass').addClass('red');
     });
+
+    $('button.slide').click(function() {
+        console.log("slide clicked");
+        $('img.slide').slideToggle('default');
+        $('img.slide').css('display','inline-block');
+    })
+
+    $('button.append').click(function(){
+        console.log("append clicked");
+        var add = $('p.addition').text();
+        $('p.original').append(add);
+    })
 
 });
